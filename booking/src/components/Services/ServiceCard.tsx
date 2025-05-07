772a611b755474421f86fe94d497dcf090f3c3a1
Joshua Ulimer Demerin <joshuaulimer.demerin-20@cpu.edu.ph>
2025-05-07 15:36:41 +0800

    remove the glow

--- booking/src/components/Services/ServiceCard.tsx
+++ booking/src/components/Services/ServiceCard.tsx
@@ -2,7 +2,6 @@ type ServiceCardProps = {
   label: string;
   icon: string;
   bgColor: string;
-  glow?: boolean;
   onClick?: () => void;
 };
 
@@ -10,14 +9,12 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
   label,
   icon,
   bgColor,
-  glow,
   onClick,
 }) => {
   return (
     <div
       onClick={onClick}
-      className={`cursor-pointer rounded-lg p-6 text-center text-xl font-bold flex flex-col items-center justify-center ${bgColor} ${
-        glow ? "shadow-lg shadow-yellow-300" : ""
+      className={`cursor-pointer rounded-lg p-6 text-center text-xl font-bold flex flex-col items-center justify-center ${bgColor} ${""
       } hover:scale-103 hover:shadow-2xl transition duration-300 ease-in-out`}
     >
       <div className="text-4xl mb-2">{icon}</div>
